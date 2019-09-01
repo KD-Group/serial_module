b382492ab86c0b5453281050d18730096230966b
Jefung <jefung865424525@gmail.com>
2019-09-01 23:29:56 +0800

    fix upload pypi error

--- setup.py
+++ setup.py
@@ -5,38 +5,9 @@
 :copyright: (c) 2018 by Jefung
 """
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 import subprocess
-
-
-def get_git_latest_tag():
-    def _minimal_ext_cmd(cmd: str):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE, env=env).communicate()[0]
-        return out
-
-    try:
-        out = _minimal_ext_cmd("git describe --abbrev=0 --tags")
-        git_tag = out.strip().decode('ascii')
-    except OSError:
-        git_tag = None
-
-    return git_tag
-
-
-latest_tag = get_git_latest_tag()
-if latest_tag is None:
-    print("get_git_latest_tag return None")
-    exit(1)
+from version import get_git_version
 
 # 读取项目下的requirements.txt
 req_txt_path = os.path.join(os.path.dirname(__file__), "requirements.txt")
@@ -54,12 +25,12 @@ def readme():
         return f.read()
 
 
-print("use latest tag as version: {}".format(latest_tag))
+print("use latest tag as version: {}".format(get_git_version()))
 print("use requirements.txt as install_requires: {}".format(req_list))
 
 setup(
     name="serial_module",
-    version=latest_tag,
+    version=get_git_version(),
     url="https://github.com/KD-Group/serial_module",
     description="串行接口模块简单包装,支持模拟接口和真实接口",
     long_description=readme(),
@@ -71,7 +42,8 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     keywords='python serial',
-    packages=["serial_module"],
+    packages=find_packages(exclude=['docs', 'tests', 'examples']),
+    # packages=["serial_module"],
     install_requires=req_list,
     python_requires='>=3',
 )
